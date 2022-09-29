0bb1f7de5399fb9ecdd9c6003f1494234c644ab4
NikkoleAshleigh <nikkole.hardy.7.1.21@gmail.com>
2022-09-29 18:56:32 -0400

    debugged my code to pass the tests, SORRY I TOOK SO LONG!

--- snippets.py
+++ snippets.py
@@ -7,12 +7,12 @@ Debug the following code snippets so that they all run successfully
 def snippet_1():
     u = 5
     v = 2
-# single = assigns, -- is equal to
+
     if u * v == 10:
         print(f"The product of u ({u}) and v ({v}) is 10")
     else:
         print(f"The product of u ({u}) and v ({v}) is not 10")
-# snippet_1()
+
 
 def snippet_2():
     x = 15
@@ -28,7 +28,7 @@ def snippet_2():
 
     else:
         print("z is greater than y")
-snippet_2()
+
 
 def snippet_3():
     # TODO: Modify the comparison operator below so the `assert` statement passes
@@ -40,16 +40,18 @@ def snippet_3():
 
     print(f"The value of c ({c}) is True since a ({a}) is equal to b ({b}).")
     assert(c == True)  # <-- DO NOT EDIT THIS LINE
+
 snippet_3()
+
 
 def snippet_4():
     # TODO: Modify exactly one boolean operator in the assignment of d, so that d evaluates to False
 
-    d = (5 > 7) or not (8 < 20)
+    d = (5 > 7) or not(8 < 20)
 
     # TODO: Explain how d is set to False in a comment
     assert(d == False)  # <-- DO NOT EDIT THIS LINE
-snippet_4()
+
 
 def snippet_5():
     # TODO: Modify the comparison operator so o evalutes to true
@@ -58,8 +60,7 @@ def snippet_5():
     m = "GOAT"
     n = "goat"
 
-    o = (m == n.upper())
+    o = (m != n)
 
-    print (f"The value of o {o} is False since Python is case-sensitive.")
+    print (f"The value of o ({o}) is True since Python is case-sensitive.")
     assert(o == True)  # <-- DO NOT EDIT THIS LINE
-snippet_5()
